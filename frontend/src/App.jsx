717e910fba75e2aa9092c05fd2d9fabfb0257d48
Avanish Upadhyay <avanishupadhyay633.com>
2025-06-04 15:34:38 +0530

    Deployment Ready

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,10 @@
+
 import './index.css'
 import {Routes, Route, BrowserRouter } from 'react-router-dom'
-import Academics from './pages/Academics'
-import GovSection from './pages/GovSection'
-import Internship from './pages/Internship'
-import Opportunities from './pages/Opportunities'
-import PersonalGuide from './pages/PersonalGuide'
-import Placement from './pages/Placement'
-import SkillDevelopment from './pages/SkillDevelopment'
-import Society from './pages/Society'
 import { Suspense } from 'react'
 import Spinner from './loaders/Spinner-avanish'
 import HomePage from './pages/HomePage'
-import News from './pages/News'
-import Feedback from './pages/Feedback'
+
 
 
 function App() {
@@ -22,7 +14,15 @@ function App() {
     <Suspense fallback={<Spinner/>}>
       <Routes>
         <Route path='/' index element={<HomePage/>}></Route>
-        <Route path='/news' element={<News/>}></Route>
+      </Routes>
+    </Suspense>
+    </BrowserRouter>
+  )
+}
+
+export default App
+/*
+<Route path='/news' element={<News/>}></Route>
         <Route path='/feedback' element={<Feedback/>}></Route>
         <Route path='/academics' element={<Academics/>}></Route>
         <Route path='/gov' element={<GovSection/>}></Route>
@@ -32,10 +32,4 @@ function App() {
         <Route path='/placement' element={<Placement/>}></Route>
         <Route path='/skilldev' element={<SkillDevelopment/>}></Route>
         <Route path='/skilldev' element={<Society/>}></Route>
-      </Routes>
-    </Suspense>
-    </BrowserRouter>
-  )
-}
-
-export default App
+ */
